ae205ff8dc2e679c3e353d599caf9418e5886e96
tetsuro.yamaguchi <tetsuro.yamaguchi@d2c.co.jp>
2021-04-13 22:45:11 +0900

    feat:MetaKey実装

--- api/domain/repository/meta.go
+++ api/domain/repository/meta.go
@@ -7,5 +7,9 @@ import (
 
 type Meta interface {
 	CreateKey(ctx context.Context, name string) (*model.MetaKey, error)
+	UpdateKey(ctx context.Context, ID uint, name string) (*model.MetaKey, error)
+	RemoveKey(ctx context.Context, ID uint) (*model.MetaKey, error)
 	FetchByName(ctx context.Context, name string) (*model.MetaKey, error)
+	FetchByID(ctx context.Context, ID uint) (*model.MetaKey, error)
+	//FetchByIDs(ctx context.Context, IDs ...uint) ([]*model.MetaKey, error)
 }
---
+++ api/handler/graph/error_converter.go
@@ -0,0 +1,49 @@
+package graph
+
+import (
+	"ao2-y/data-tag-manager/handler/graph/model"
+	"ao2-y/data-tag-manager/usecase"
+	"errors"
+	"fmt"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+)
+
+func createError(msg string, err error) error {
+	// 独自Error型から対応するコードに変換して返す
+	var validationError *usecase.ValidationError
+	var resourceNotFoundError *usecase.ResourceNotFoundError
+	var internalServerError *usecase.InternalServerError
+	if errors.As(err, &validationError) {
+		// バリデーションエラー
+		return &gqlerror.Error{
+			Message: fmt.Sprintf("%s. %v", msg, err),
+			Extensions: map[string]interface{}{
+				"code":   model.ErrorValidationError,
+				"field:": validationError.Field,
+			},
+		}
+	}
+	if errors.As(err, &resourceNotFoundError) {
+		// リソースが存在しなかった
+		return &gqlerror.Error{
+			Message: err.Error(),
+			Extensions: map[string]interface{}{
+				"code": model.ErrorResourceNotFound,
+			},
+		}
+	}
+	if errors.As(err, &internalServerError) {
+		return &gqlerror.Error{
+			Message: fmt.Sprintf("%s. %v", msg, err),
+			Extensions: map[string]interface{}{
+				"code": model.ErrorInternalServerError,
+			},
+		}
+	}
+	return &gqlerror.Error{
+		Message: fmt.Sprintf(msg, err),
+		Extensions: map[string]interface{}{
+			"code": model.ErrorInternalServerError,
+		},
+	}
+}
