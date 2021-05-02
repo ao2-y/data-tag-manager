deb495243bbfedc3fff3705c8371228e87dc7788
Tetsu Yamaguchi <mint.rabi@gmail.com>
2021-05-02 22:24:57 +0900

    fix:lint

--- api/infra/persistent/mysql/meta.go
+++ api/infra/persistent/mysql/meta.go
@@ -15,30 +15,28 @@ type metaRepository struct {
 	cache inmemory.Cache
 }
 
-func (m *metaRepository) FetchByID(ctx context.Context, ID uint) (*model.MetaKey, error) {
-
-	if ret := m.cache.Restore(m.cacheKeyID(ID)); ret != nil {
+func (m *metaRepository) FetchByID(ctx context.Context, iD uint) (*model.MetaKey, error) {
+	if ret := m.cache.Restore(m.cacheKeyID(iD)); ret != nil {
 		if meta, ok := ret.(*model.MetaKey); ok {
 			return meta, nil
 		}
 	}
 
-	metaKeys := &MetaKeys{ID: ID}
+	metaKeys := &MetaKeys{ID: iD}
 	err := m.db.WithContext(ctx).First(metaKeys).Error
 	if err != nil {
-		if err == gorm.ErrRecordNotFound {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, repository.NewOperationError(repository.ErrNotFound, nil)
 		}
 		return nil, repository.NewOperationError(repository.ErrUnknown, err)
 	}
 
 	modelMeta := metaKeyToDomain(metaKeys)
-	m.cache.Store(m.cacheKeyID(ID), modelMeta)
+	m.cache.Store(m.cacheKeyID(iD), modelMeta)
 	return modelMeta, nil
 }
 
 func (m *metaRepository) FetchByName(ctx context.Context, name string) (*model.MetaKey, error) {
-
 	if ret := m.cache.Restore(m.cacheKeyName(name)); ret != nil {
 		if meta, ok := ret.(*model.MetaKey); ok {
 			return meta, nil
@@ -75,10 +73,9 @@ func (m *metaRepository) CreateKey(ctx context.Context, name string) (*model.Met
 	return modelMeta, nil
 }
 
-func (m *metaRepository) UpdateKey(ctx context.Context, ID uint, name string) (*model.MetaKey, error) {
-
+func (m *metaRepository) UpdateKey(ctx context.Context, iD uint, name string) (*model.MetaKey, error) {
 	metaKey := &MetaKeys{
-		ID: ID,
+		ID: iD,
 	}
 	tx := m.db.WithContext(ctx).Begin()
 	err := tx.WithContext(ctx).Set("gorm:query_option", "FOR UPDATE").First(metaKey).Error
@@ -88,7 +85,7 @@ func (m *metaRepository) UpdateKey(ctx context.Context, ID uint, name string) (*
 	}
 
 	// 古いキャッシュを削除
-	m.cache.Delete(m.cacheKeyID(ID))
+	m.cache.Delete(m.cacheKeyID(iD))
 	m.cache.Delete(m.cacheKeyName(metaKey.Name))
 
 	metaKey.Name = name
@@ -101,13 +98,12 @@ func (m *metaRepository) UpdateKey(ctx context.Context, ID uint, name string) (*
 	tx.WithContext(ctx).Commit()
 
 	modelMeta := metaKeyToDomain(metaKey)
-	m.cache.Store(m.cacheKeyID(ID), modelMeta)
+	m.cache.Store(m.cacheKeyID(iD), modelMeta)
 	m.cache.Store(m.cacheKeyName(name), modelMeta)
 	return modelMeta, nil
 }
 
 func (m *metaRepository) RemoveKey(ctx context.Context, ID uint) (*model.MetaKey, error) {
-
 	metaKey := &MetaKeys{
 		ID: ID,
 	}
@@ -131,8 +127,8 @@ func (m *metaRepository) RemoveKey(ctx context.Context, ID uint) (*model.MetaKey
 	return metaKeyToDomain(metaKey), nil
 }
 
-func (m *metaRepository) cacheKeyID(ID uint) string {
-	return fmt.Sprintf("MetaKeyID:%v", ID)
+func (m *metaRepository) cacheKeyID(iD uint) string {
+	return fmt.Sprintf("MetaKeyID:%v", iD)
 }
 
 func (m *metaRepository) cacheKeyName(name string) string {
